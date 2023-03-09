5d6c3bce5b3101d20214f93b9f091eeba1d051c8
adrielle-b <adriielle@gmail.com>
2023-03-09 18:51:13 -0300

    requisitos 4 e 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,31 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const todosAnimais = () => {
+  const qtdAnimais = species.reduce((novoValor, { name, residents }) =>
+    ({ ...novoValor, [name]: residents.length }), {});
+  return qtdAnimais;
+};
+
+const qtdAnimalParamentro = (animal) => {
+  const qtdAnimal = data.species
+    .find((elemento) => elemento.name === animal.species).residents.length;
+  return qtdAnimal;
+};
+
+const qtdAnimalGenero = (animal) => {
+  const qtdGenero = data.species.find((elemento) => elemento.name === animal.species);
+  return qtdGenero.residents.filter((elemento) => elemento.sex === animal.sex).length;
+};
+
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    return todosAnimais();
+  }
+  if (Object.values(animal).length === 1) {
+    return qtdAnimalParamentro(animal);
+  }
+  return qtdAnimalGenero(animal);
 };
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,14 @@
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const isManager = (id) => data.employees.some((pessoa) => pessoa.managers.includes(id));
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    const isGerenciadas = data.employees.filter((pessoa) => pessoa.managers.includes(managerId));
+    return isGerenciadas.map((colaborador) => `${colaborador.firstName} ${colaborador.lastName}`);
+  }
 };
 
 module.exports = { isManager, getRelatedEmployees };
