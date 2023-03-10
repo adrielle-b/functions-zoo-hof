364a32062c5177bc3b50893db060ae72913fe826
adrielle-b <adriielle@gmail.com>
2023-03-10 16:00:45 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const colaborador = data.employees.find((pessoa) => pessoa.id === id);
+  const especieResponsavel = colaborador.responsibleFor[0];
+  const especie = data.species.find((animal) => especieResponsavel.includes(animal.id));
+  const animaisOrdenados = especie.residents.sort((animal1, animal2) => {
+    if (animal1.age > animal2.age) { return -1; }
+    if (animal1.age < animal2.age) { return 1; }
+    return 0;
+  });
+  const maisVelho = Object.values(animaisOrdenados[0]);
+  return maisVelho;
 };
 
 module.exports = getOldestFromFirstSpecies;
