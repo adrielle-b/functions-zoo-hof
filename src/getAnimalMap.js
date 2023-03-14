34fa9b1e8809f79d7128c800625cbe37b3d2691f
adrielle-b <adriielle@gmail.com>
2023-03-13 22:30:50 -0300

    requisitos 11 e 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,47 @@
 const data = require('../data/zoo_data');
 
+const filterLocation = (local) => {
+  const especies = data.species.filter((animal) =>
+    animal.location === local).map(({ name }) => name);
+  return especies;
+};
+
+const especiesLocations = () => {
+  const objetoLocation = {
+    NE: filterLocation('NE'),
+    NW: filterLocation('NW'),
+    SE: filterLocation('SE'),
+    SW: filterLocation('SW'),
+  };
+  return objetoLocation;
+};
+
+const relatorioNames = (sorted, sex) => data.species
+  .reduce((novoValor, { location, residents, name }) => {
+    const novoObj = { ...novoValor };
+    if (!novoObj[location]) {
+      novoObj[location] = [];
+    }
+    let animaisDaEspecie = residents;
+    if (sex) {
+      animaisDaEspecie = animaisDaEspecie.filter((animal) => animal.sex === sex);
+    }
+    animaisDaEspecie = animaisDaEspecie.map((animal) => animal.name);
+    if (sorted === true) {
+      animaisDaEspecie.sort();
+    }
+    novoObj[location].push({ [name]: animaisDaEspecie });
+    return novoObj;
+  }, {});
+
 const getAnimalMap = (options) => {
-  // seu código aqui
+  if (!options) {
+    return especiesLocations();
+  }
+  if (!options.includeNames) {
+    return especiesLocations();
+  }
+  return relatorioNames(options.sorted, options.sex);
 };
 
 module.exports = getAnimalMap;
--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const novoRelatorio = (colaborador) => {
+  const animais = data.species.filter((animal) => colaborador.responsibleFor.includes(animal.id));
+  const nomesAnimais = animais.map(({ name }) => name);
+  const localizacaoAnimais = animais.map(({ location }) => location);
+  return {
+    id: colaborador.id,
+    fullName: `${colaborador.firstName} ${colaborador.lastName}`,
+    species: nomesAnimais,
+    locations: localizacaoAnimais,
+  };
+};
+
+const relatorioTodosEmployees = () => data.employees
+  .map((colaborador) => novoRelatorio(colaborador));
+
+const getEmployeesCoverage = (param) => {
+  const todosRelatorios = relatorioTodosEmployees();
+  if (!param) return todosRelatorios;
+  if (todosRelatorios.some(({ fullName }) => fullName.includes(param.name))) {
+    return todosRelatorios.find(({ fullName }) => fullName.includes(param.name));
+  }
+  if (todosRelatorios.some(({ id }) => id === param.id)) {
+    return todosRelatorios.find(({ id }) => id === param.id);
+  }
+  throw new Error('Informações inválidas');
 };
 
 module.exports = getEmployeesCoverage;
